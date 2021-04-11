ea5e8d9f96fe4c47e0b26fabaed257c93b5dc4cf
Sahitya2006 <78652756+Sahitya2006@users.noreply.github.com>
2021-04-11 21:23:29 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,28 @@
+class Roof{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var roofPos=this.body.position;		
+
+			push()
+			translate(roofPos.x, roofPos.y,width,height);
+			rectMode(CENTER)
+			fill ("white");
+			rect(roofPos.x,roofPos.y,this.w, this.h);
+			pop();
+	}
+
+}
